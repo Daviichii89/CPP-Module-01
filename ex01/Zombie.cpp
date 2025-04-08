63ff85188ed545387a20262682c8f23bace8f819
David Fernandez <davichi89@gmail.com>
2025-04-08 22:14:57 +0200

    create zombie class and zombieHorde function

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,11 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : name(name) {}
+
+void Zombie::announce() {
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie() {
+    std::cout << name << " is destroyed." << std::endl;
+}
