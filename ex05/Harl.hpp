fbeace88339a61ce703d66bb2eda81d25947c04b
David Fernandez <davichi89@gmail.com>
2025-04-20 19:19:47 +0200

    Finish ex05

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -4,11 +4,11 @@
 #include <iostream>
 #include <string>
 
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
 #define BLUE "\033[34m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
 #define RESET "\033[0m"
+#define YELLOW "\033[33m"
 
 class Harl {
     private:
