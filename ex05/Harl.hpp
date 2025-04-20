666cbefae0171d89f35ec7837029990ae78597a6
David Fernandez <davichi89@gmail.com>
2025-04-20 19:19:10 +0200

    Finish ex04

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -4,14 +4,20 @@
 #include <iostream>
 #include <string>
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define RESET "\033[0m"
+
 class Harl {
-    public:
-        void complain( std::string level );
     private:
-        void debug( void );
-        void info( void );
-        void warning( void );
-        void error( void );
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
 };
 
 #endif
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -3,15 +3,10 @@
 int main() {
     Harl harl;
 
-    std::cout << "Choose a level to complain about:" << std::endl;
-    std::cout << "DEBUG, INFO, WARNING, ERROR" << std::endl;
-
-    std::cout << "Enter level: ";
-    std::string level;
-
-    std::cin >> level;
-
-    harl.complain(level);
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
 
     return 0;
 }
